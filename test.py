7197129dfe75945bc6e5d9fe3fc56fbadcfb5ba9
massino <nacerddine.amassin@gmail.com>
2022-02-02 05:37:31 +0100

    recherche amélioré

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 
-import wikipediaapi
-wiki_wiki = wikipediaapi.Wikipedia('fr')
+import wikipedia
+import random
+
+wikipedia.set_lang("fr")
 
 def recherche(nom_prenom_depute):
-
-    page_wiki = wiki_wiki.page(nom_prenom_depute)
-    print(page_wiki.title)
-    print( page_wiki.summary)
+    page_wiki = wikipedia.page(nom_prenom_depute)
+    print(page_wiki.summary)
+    print(page_wiki.url)
+    n = random.randint(3, len(page_wiki.images))
+    print(page_wiki.images[n])
+#'https://upload.wikimedia.org/wikipedia/commons/7/7e/Circle-icons-profile.svg'
 
 
 recherche("Véronique Louwagie")
